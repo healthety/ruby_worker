16579a9cfbb17ddec758c94f7322716a536f6cf7
Martin Jagusch <m@martinjagusch.com>
2011-04-13 12:29:26 +0200

    Extend worker spec

--- spec/unit/worker_spec.rb
+++ spec/unit/worker_spec.rb
@@ -1,19 +1,28 @@
 require "spec_helper"
 
-describe Worker do
-  before(:each) do
-    @block = Proc.new { interval 0.5; value 10 }
-    @worker = Worker.new(:test, &@block)
-  end
+module Healthety
+  describe Worker do
+    before(:each) do
+      @block = Proc.new { interval 0.5; value 10 }
+      @worker = Worker.new(:test, &@block)
+    end
 
-  it "should initialize" do
-    @worker.name.should eq("test")
-    @worker.instance_variable_get(:@block).should be(@block)
-  end
+    it "should initialize" do
+      @worker.name.should eq("test")
+      @worker.instance_variable_get(:@block).should be(@block)
+    end
 
-  it "should perform" do
-    @worker.perform
-    @worker._interval.should eq(0.5)
-    @worker._value.should eq("10")
+    it "should perform" do
+      @worker.perform
+
+      @worker.interval.should eq(0.5)
+      @worker.value.should eq(10)
+
+      @worker.interval 0.7
+      @worker.value 11
+
+      @worker.interval.should eq(0.7)
+      @worker.value.should eq(11)
+    end
   end
 end
