c29037e8bb7ab1278f1d4dc8796d6bb365b7dab0
Hans Hasselberg <hans.hasselberg@kaeuferportal.de>
2011-04-21 10:21:40 +0200

    add hostname to json

--- lib/healthety/transmission.rb
+++ lib/healthety/transmission.rb
@@ -6,11 +6,12 @@ module Healthety
     def initialize(server, port)
       @server = server
       @port = port
+      @host = Socket.gethostname
       @socket = UDPSocket.new
     end
 
     def send(name, value)
-      data = {:name => name, :value => value}.to_json
+      data = {:name => name, :value => value, :host => @host}.to_json
       @socket.send(data, 0, @server, @port)
       $stdout << "#{name}: #{value}\n"
     end
